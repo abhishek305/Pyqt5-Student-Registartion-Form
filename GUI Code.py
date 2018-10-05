66bb581021d131dd55905763a891a00addd1cb71
Abhishek Ezhava <Abhishekshaji32@gmail.com>
2018-10-05 12:13:08 +0530

    Update GUI Code.py

--- GUI Code.py
+++ GUI Code.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'ex.ui'
-#
-# Created by: PyQt5 UI code generator 5.8.2
-#
-# WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
